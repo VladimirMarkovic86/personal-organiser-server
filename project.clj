112ad5f74c314ef892664e898b85302b5047006e
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-08-21 19:32:24 +0200

    Reformatting project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject personal-organiser-server "0.1.0"
+(defproject org.vladimir/personal-organiser-server "0.1.0"
   :description "Personal organiser server"
   :url "http://gitlab:1610/VladimirMarkovic86/personal-organiser-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure			   "1.9.0"]
-         									;	https://clojure.org/api/api
+  :dependencies [[org.clojure/clojure	"1.9.0"]
          								[org.vladimir/server-lib "0.1.0"]
          								[org.vladimir/mongo-lib "0.1.0"]
          								[org.vladimir/utils-lib "0.1.0"]
@@ -16,4 +15,5 @@
   ; AOT - Compailation ahead of time
   :main ^:skip-aot personal-organiser-server.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :repl-options {:port 8601})
