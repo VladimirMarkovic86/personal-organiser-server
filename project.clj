e078c5722dd6fbc15d82e1fcdd038762d16c652f
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-15 17:47:15 +0200

    Prepared project for clojars.

--- project.clj
+++ project.clj
@@ -1,16 +1,18 @@
-(defproject org.vladimir/personal-organiser-server "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/personal-organiser-server "0.1.0"
   :description "Personal organiser server"
-  :url "http://gitlab:1610/VladimirMarkovic86/personal-organiser-server"
+  :url "http://github.com/VladimirMarkovic86/personal-organiser-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.vladimir/server-lib "0.1.0"]
-                 [org.vladimir/db-lib "0.1.0"]
-                 [org.vladimir/ajax-lib "0.1.0"]
-                 [org.vladimir/session-lib "0.1.0"]
-                 [org.vladimir/common-server "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/db-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/common-server "0.1.0"]
                  ]
 
+  :min-lein-version "2.0.0"
+  
   ; AOT - Compailation ahead of time
   :main ^:skip-aot personal-organiser-server.core
   :target-path "target/%s"
