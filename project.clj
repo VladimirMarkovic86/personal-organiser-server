394ee4bc191f97855966e9adc608ac08d8344ef1
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-25 15:57:36 +0200

    Changed version of utils-lib, which has java-heap-size. Changed version of server-lib.

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/server-lib "0.3.0"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.3.1"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.0"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.0"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.0"]
                  [org.clojars.vladimirmarkovic86/common-server "0.2.0"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.2.0"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.3.0"]
                  ]
 
   :min-lein-version "2.0.0"
