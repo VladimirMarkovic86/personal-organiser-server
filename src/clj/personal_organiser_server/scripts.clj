91dfca2e70e6a1b09eb17f3f08e4b46aa258e3ca
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-31 12:08:29 +0100

    Added comments where it was needed.

--- src/clj/personal_organiser_server/scripts.clj
+++ src/clj/personal_organiser_server/scripts.clj
@@ -3,7 +3,7 @@
             [utils-lib.core :as utils]))
 
 (defn initialize-db
-  ""
+  "Initialize database"
   []
   (mon/mongodb-insert-many
     "language"
@@ -123,7 +123,7 @@
  )
 
 (defn initialize-db-if-needed
-  ""
+  "Check if database exists and initialize it if it doesn't"
   []
   (try
     (when-not (mon/mongodb-exists
