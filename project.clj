2b5f55b0fc087540b4bc2bc751c4d68a86c98bcd
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-11-28 14:30:03 +0100

    Task number: 3 Refactored md5 checksum generation to work only with first 1024 bytes or less if file is smaller then 1024 bytes.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/server-lib "0.3.11"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.3.12"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.2"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.1"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.4"]
