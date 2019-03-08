a9db990fb1e74c6a3593d23f7d276cccc2a016c9
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-03-08 17:30:12 +0100

    Task 62: Refactored common-server, separated configuration for server and client side.

---
+++ src/clj/personal_organiser_server/config.clj
@@ -0,0 +1,73 @@
+(ns personal-organiser-server.config
+  (:require [ajax-lib.http.response-header :as rsh]
+            [server-lib.core :as srvr]))
+
+(def db-uri
+     (or (System/getenv "MONGODB_URI")
+         (System/getenv "PROD_MONGODB")
+         "mongodb://admin:passw0rd@127.0.0.1:27017/admin"))
+
+(def db-name
+     "personal-organiser-db")
+
+(defn define-port
+  "Defines server's port"
+  []
+  (let [port (System/getenv "PORT")
+        port (if port
+               (read-string
+                 port)
+               1601)]
+    port))
+
+(defn build-access-control-map
+  "Build access control map"
+  []
+  (let [access-control-allow-origin #{"https://personal-organiser:8443"
+                                      "https://personal-organiser:1611"
+                                      "http://personal-organiser:1611"
+                                      "https://personal-organiser:1601"
+                                      "http://personal-organiser:1601"
+                                      "https://192.168.1.86:1611"
+                                      "http://192.168.1.86:1611"
+                                      "https://192.168.1.86:1601"
+                                      "http://192.168.1.86:1601"
+                                      "http://personal-organiser:8445"}
+        access-control-allow-origin (if (System/getenv "CLIENT_ORIGIN")
+                                      (conj
+                                        access-control-allow-origin
+                                        (System/getenv "CLIENT_ORIGIN"))
+                                      access-control-allow-origin)
+        access-control-allow-origin (if (System/getenv "SERVER_ORIGIN")
+                                      (conj
+                                        access-control-allow-origin
+                                        (System/getenv "SERVER_ORIGIN"))
+                                      access-control-allow-origin)
+        access-control-map {(rsh/access-control-allow-origin) access-control-allow-origin
+                            (rsh/access-control-allow-methods) "OPTIONS, GET, POST, DELETE, PUT"
+                            (rsh/access-control-allow-credentials) true}]
+    access-control-map))
+
+(defn build-certificates-map
+  "Build certificates map"
+  []
+  (let [certificates {:keystore-file-path
+                       "certificate/personal_organiser_server.jks"
+                      :keystore-password
+                       "ultras12"}
+        certificates (when-not (System/getenv "CERTIFICATES")
+                       certificates)]
+    certificates))
+
+(defn set-thread-pool-size
+  "Set thread pool size"
+  []
+  (let [thread-pool-size (System/getenv "THREAD_POOL_SIZE")]
+    (when thread-pool-size
+      (reset!
+        srvr/thread-pool-size
+        (read-string
+          thread-pool-size))
+     ))
+ )
+
--- src/clj/personal_organiser_server/core.clj
+++ src/clj/personal_organiser_server/core.clj
@@ -3,28 +3,21 @@
   (:require [session-lib.core :as ssn]
             [server-lib.core :as srvr]
             [mongo-lib.core :as mon]
+            [personal-organiser-server.config :as config]
             [personal-organiser-server.scripts :as scripts]
             [personal-organiser-middle.request-urls :as prurls]
             [personal-organiser-middle.functionalities :as pomfns]
             [common-server.core :as rt]
-            [ajax-lib.http.response-header :as rsh]
             [ajax-lib.http.entity-header :as eh]
             [ajax-lib.http.mime-type :as mt]
             [ajax-lib.http.status-code :as stc]
+            [ajax-lib.http.request-method :as rm]
             [utils-lib.core :refer [parse-body]]
             [audit-lib.core :refer [audit]]
             [personal-organiser-middle.collection-names :refer [grocery-cname
                                                                 meal-cname
                                                                 organism-cname]]
             [personal-organiser-lib.core :as pol]))
-
-(def db-uri
-     (or (System/getenv "MONGODB_URI")
-         (System/getenv "PROD_MONGODB")
-         "mongodb://admin:passw0rd@127.0.0.1:27017/admin"))
-
-(def db-name
-     "personal-organiser-db")
 
 (defn get-meals-with-groceries
   "Returns meals from database with data of groceries"
@@ -94,11 +87,13 @@
 
 (defn calculate-meal-recommendations
   "Calculates meal recommendations for particular organism"
-  [{organism-id :organism-id
-    breakfast-calories-share :breakfast-calories-share
-    lunch-calories-share :lunch-calories-share
-    dinner-calories-share :dinner-calories-share}]
-  (let [organism (mon/mongodb-find-by-id
+  [request]
+  (let [{organism-id :organism-id
+         breakfast-calories-share :breakfast-calories-share
+         lunch-calories-share :lunch-calories-share
+         dinner-calories-share :dinner-calories-share} (parse-body
+                                                         request)
+        organism (mon/mongodb-find-by-id
                    organism-cname
                    organism-id)
         meals (get-meals-with-groceries)
@@ -115,55 +110,25 @@
               :data meals-recommendations})})
  )
 
-(defn response-routing-fn
-  "Custom routing function"
-  [request]
-  (let [{request-uri :request-uri
-         request-method :request-method} request]
-    (cond
-      (= request-method
-         "POST")
-        (cond
-          (= request-uri
-             prurls/calculate-meal-recommendations-url)
-            (calculate-meal-recommendations
-              (parse-body
-                request))
-          :else
-            nil)
-      :else
-        nil))
- )
+(def logged-in-routing-set
+  (atom
+    #{{:method rm/POST
+       :uri prurls/calculate-meal-recommendations-url
+       :authorization pomfns/meal-recommendation
+       :action calculate-meal-recommendations}}))
 
-(defn allow-action-routing-fn
-  "Check if action is allowed for logged in user"
-  [request]
-  (let [allowed-functionalities (rt/get-allowed-actions
-                                  request)
-        {request-uri :request-uri
-         request-method :request-method} request]
-    (cond
-      (= request-method
-         "POST")
-        (cond
-          (= request-uri
-             prurls/calculate-meal-recommendations-url)
-            (contains?
-              allowed-functionalities
-              pomfns/meal-recommendation)
-          :else
-            false)
-      :else
-        false))
- )
+(def logged-out-routing-set
+  (atom
+    #{}))
 
 (defn routing
   "Custom routing function"
   [request]
+  (rt/add-new-routes
+    @logged-in-routing-set
+    @logged-out-routing-set)
   (let [response (rt/routing
-                   request
-                   response-routing-fn
-                   allow-action-routing-fn)]
+                   request)]
     (audit
       request
       response)
@@ -173,55 +138,18 @@
   "Start server"
   []
   (try
-    (let [port (System/getenv "PORT")
-          port (if port
-                 (read-string
-                   port)
-                 1601)
-          access-control-allow-origin #{"https://personal-organiser:8443"
-                                        "https://personal-organiser:1611"
-                                        "http://personal-organiser:1611"
-                                        "https://personal-organiser:1601"
-                                        "http://personal-organiser:1601"
-                                        "https://192.168.1.86:1611"
-                                        "http://192.168.1.86:1611"
-                                        "https://192.168.1.86:1601"
-                                        "http://192.168.1.86:1601"
-                                        "http://personal-organiser:8445"}
-          access-control-allow-origin (if (System/getenv "CLIENT_ORIGIN")
-                                        (conj
-                                          access-control-allow-origin
-                                          (System/getenv "CLIENT_ORIGIN"))
-                                        access-control-allow-origin)
-          access-control-allow-origin (if (System/getenv "SERVER_ORIGIN")
-                                        (conj
-                                          access-control-allow-origin
-                                          (System/getenv "SERVER_ORIGIN"))
-                                        access-control-allow-origin)
-          access-control-map {(rsh/access-control-allow-origin) access-control-allow-origin
-                              (rsh/access-control-allow-methods) "OPTIONS, GET, POST, DELETE, PUT"
-                              (rsh/access-control-allow-credentials) true}
-          certificates {:keystore-file-path
-                         "certificate/personal_organiser_server.jks"
-                        :keystore-password
-                         "ultras12"}
-          certificates (when-not (System/getenv "CERTIFICATES")
-                         certificates)
-          thread-pool-size (System/getenv "THREAD_POOL_SIZE")]
-      (when thread-pool-size
-        (reset!
-          srvr/thread-pool-size
-          (read-string
-            thread-pool-size))
-       )
+    (let [port (config/define-port)
+          access-control-map (config/build-access-control-map)
+          certificates-map (config/build-certificates-map)]
+      (config/set-thread-pool-size)
       (srvr/start-server
         routing
         access-control-map
         port
-        certificates))
+        certificates-map))
     (mon/mongodb-connect
-      db-uri
-      db-name)
+      config/db-uri
+      config/db-name)
     (scripts/initialize-db-if-needed)
     (ssn/create-indexes)
     (catch Exception e
