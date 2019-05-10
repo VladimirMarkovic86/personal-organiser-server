98fd7f363d142e6960160210bbc1e13fe29cb8ae
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-05-10 16:32:15 +0200

    Task 63: Implemented tests for personal-organiser-lib.core namespace.

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.6"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.8"]
                  [org.clojars.vladimirmarkovic86/audit-lib "0.1.25"]
-                 [org.clojars.vladimirmarkovic86/personal-organiser-lib "0.1.3"]
+                 [org.clojars.vladimirmarkovic86/personal-organiser-lib "0.1.4"]
                  [org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.2"]
                  ]
 
